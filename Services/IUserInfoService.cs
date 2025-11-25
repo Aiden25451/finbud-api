e90bc8da74fa9d2aa06851c3d88a82448edf1538
Michelle C <93887236+MicheC28@users.noreply.github.com>
2025-11-24 21:49:56 -0500

    add user profile picture field (#9)
    add user profile picture field to user info table and remove user id
    from user info response.
    
    Added update profile pic endpoint.
    Updated get user info endpoint to include user profile pic.

--- Services/IUserInfoService.cs
+++ Services/IUserInfoService.cs
@@ -7,5 +7,6 @@ public interface IUserInfoService
     Task<GetUserInfoDTO?> CreateUserInfoAsync(CreateUserInfoDTO request, string userId);
     Task<GetUserInfoDTO?> GetUserInfoByUserIdAsync(string userId);
     Task<GetUserInfoDTO?> UpdateUserInfoUsernameAsync(UpdateUsernameDTO request, string userId);
+    Task<GetUserInfoDTO?> UpdateUserInfoUserProfilePictureAsync(UpdateUserProfilePictureDTO request, string userId);
     Task<bool> DeleteUserInfoAsync(string userId);
 }
